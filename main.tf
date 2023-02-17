d2394d3075cc655ec6a01805be5aaff31cfceef0
Chakra Thangavelu <chakraodba@gmail.com>
2023-02-17 13:29:55 +0530

    started demo project

--- main.tf
+++ main.tf
@@ -2,45 +2,30 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-variable "aws_vpc_cidr" {
-  type = list(object({
-    cidr_block = string,
-    name = string
-}))
-}
-variable "tag_name" {}
+variable "aws_vpc_cidr" {}
+variable "aws_vpc_subnet_cidr" {}
+variable "env" {}
+variable "avail_zone" {}
+
 
 resource "aws_vpc" "myvpc" {
-    cidr_block = var.aws_vpc_cidr[0].cidr_block
+    cidr_block = var.aws_vpc_cidr
     tags = {
-      "Name" = var.aws_vpc_cidr[0].name
+      "Name" = "{var.env}-vpc"
     }
   
 }
 
 resource "aws_subnet" "myvpc-subnet-public" {
     vpc_id = aws_vpc.myvpc.id
-    cidr_block = var.aws_vpc_cidr[0].cidr_block
-    availability_zone = "ap-south-1a"
+    cidr_block = var.aws_vpc_subnet_cidr
+    availability_zone = var.avail_zone
     tags = {
-      "Name" = var.aws_vpc_cidr[0].name
+      "Name" = "{var.env}-subnet"
     }
   
 }
-
-data "aws_vpc" "defaultone" {
-    default = true
-}
-
-resource "aws_subnet" "default-subnet-public" {
-    vpc_id = data.aws_vpc.defaultone.id
-    cidr_block = "172.31.16.0/28"
-    availability_zone = "ap-south-1b"
-    tags = {
-      "Name" = "default-sub-pub"
-    }
-}
-  
+ 
 output "aws_vpc_id" {
   value = aws_vpc.myvpc.id
 }
