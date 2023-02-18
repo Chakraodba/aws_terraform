2f0edf89393ace963757221200e34dfa8857feb4
Chakra Thangavelu <chakraodba@gmail.com>
2023-02-18 21:28:51 +0530

    added stage variable file

--- data.sh
+++ data.sh
@@ -4,6 +4,6 @@ sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
-docker run -p 8080:80 nginx
+sudo docker run -p 8080:80 nginx
 sudo yum install openssh -y 
 sudo service sshd restart
